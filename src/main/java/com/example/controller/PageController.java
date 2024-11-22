a50a561e10914277935bea4e8ab6fd23fc1370d8
何宇帆 <15121832+he-yu_fan@user.noreply.gitee.com>
2024-11-22 18:35:39 +0800

    薪酬管理

--- src/main/java/com/example/controller/PageController.java
+++ src/main/java/com/example/controller/PageController.java
@@ -21,30 +21,6 @@ public class PageController {
         return "reset_password"; // 返回 reset_password.jsp
     }
 
-    // 显示薪酬标准列表页面
-    @GetMapping("/SalaryStandardList")
-    public String salaryStandardListPage() {
-        return "SalaryStandardList"; // 返回 SalaryStandardList.jsp
-    }
-
-    // 显示创建薪酬标准页面
-    @GetMapping("/SalaryStandardCreate")
-    public String salaryStandardCreatePage() {
-        return "SalaryStandardCreate"; // 返回 SalaryStandardCreate.jsp
-    }
-
-    // 显示审核薪酬标准页面
-    @GetMapping("/SalaryStandardReview")
-    public String salaryStandardReviewPage() {
-        return "SalaryStandardReview"; // 返回 SalaryStandardReview.jsp
-    }
-
-    // 显示搜索薪酬标准页面
-    @GetMapping("/SalaryStandardSearch")
-    public String salaryStandardSearchPage() {
-        return "SalaryStandardSearch"; // 返回 SalaryStandardSearch.jsp
-    }
-
     @GetMapping("/SalaryStandard")
     public String salaryStandardPage() {
         return "SalaryStandard";
